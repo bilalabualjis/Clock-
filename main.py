5f75156165b0263855fac54bfe7c9765e03b50c7
bilal <abualjisbilal@gmail.com>
2021-03-29 23:03:00 -0700

    Added comments for readability

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from tkinter.ttk import *
 
 from time import strftime
 
+# Creating a root window
 root = Tk()
+
+# Adding a title to the window
 root.title("Clock")
 
+# Defining a time function
 def time():
     string = strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)
+
 
 label = Label(root, font=("ds-digital", 80), background="blue", foreground= "black")
 label.pack(anchor='center')
